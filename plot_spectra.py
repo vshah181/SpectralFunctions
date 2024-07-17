dc1772909a05c52941ce5daed26f319cf0f92a0f
Varun Shah <varun.shah@postgrad.manchester.ac.uk>
2024-07-17 14:35:51 +0100

    Add some comments and docstrings like a good lil nerd

--- plot_spectra.py
+++ plot_spectra.py
@@ -1,22 +1,35 @@
-import numpy as np
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 def read_layer_index(nlayers, min_layers):
+    """
+    Read the required layers from the user input.
+    :param nlayers: Number of layers in the modelled system (integer).
+    :param min_layers: Minimum acceptable layer index (integer).
+    :return: layer_index: the layer we need to draw a plot for (integer).
+    """
     user_input = input('Enter the layer for projection: ')
-                       
     try:
         layer_index = int(user_input)
         if layer_index > nlayers or layer_index < min_layers:
-            sys.exit('FATAL ERROR: Input is out of range. Must be greater '\
-                    f'than {min_layers-1} and less than {nlayers+1}.')
+            sys.exit('FATAL ERROR: Input is out of range. Must be greater '
+                     f'than {min_layers-1} and less than {nlayers+1}.')
     except ValueError:
         sys.exit('FATAL ERROR: Input is not a number.')
     return layer_index
 
 
 def make_energy_array(emin, emax, de, efermi):
+    """
+    Create the energy array for the plot
+    :param emin: minimum energy, ie: ymin (float)
+    :param emax: maximum energy, ie: ymax (float)
+    :param de: Energy step (float)
+    :param efermi: Fermi level (float)
+    :return: array of energies: ndarray(dtype=float)
+    """
     nene = int((emax - emin) / de)
     energy_window = np.empty(nene)
     for i in range(nene):
@@ -25,7 +38,11 @@ def make_energy_array(emin, emax, de, efermi):
 
 
 def read_master_input():
-    f = open('INPUT', 'r')
+    """
+    Read the master input file
+    :return: nlayer (int), energy_array (1darray(dtype=float)),
+             seedname (string), fig_dimensions (1darray(dtype=float))
+    """
     nlayers = 0
     fermi_level = 0.0
     emin = 0.0
@@ -33,41 +50,50 @@ def read_master_input():
     energy_step = 1.0
     seedname = 'seedname'
     fig_dimensions = np.empty([2])
-    for line in f:
-        split_line = line.split()
-        if split_line[0] == 'nlayers':
-            nlayers = int(split_line[1])
-        elif split_line[0] == 'e_fermi':
-            fermi_level = float(split_line[1])
-        elif split_line[0] == 'energy_step':
-            energy_step = float(split_line[1])
-        elif split_line[0] == 'energy_range':
-            emin = float(split_line[1])
-            emax = float(split_line[2])
-        elif split_line[0] == 'seedname':
-            seedname = split_line[1]
-        elif split_line[0] == 'figsize':
-            fig_dimensions = (float(split_line[1]), float(split_line[2]))
-    f.close()
+    with open('INPUT', 'r', encoding='utf-8') as f:
+        for line in f:
+            split_line = line.split()
+            if split_line[0] == 'nlayers':
+                nlayers = int(split_line[1])
+            elif split_line[0] == 'e_fermi':
+                fermi_level = float(split_line[1])
+            elif split_line[0] == 'energy_step':
+                energy_step = float(split_line[1])
+            elif split_line[0] == 'energy_range':
+                emin = float(split_line[1])
+                emax = float(split_line[2])
+            elif split_line[0] == 'seedname':
+                seedname = split_line[1]
+            elif split_line[0] == 'figsize':
+                fig_dimensions = (float(split_line[1]), float(split_line[2]))
     energy_array = make_energy_array(emin, emax, energy_step, fermi_level)
-    return nlayers, fermi_level, energy_array, seedname, fig_dimensions
+    return nlayers, energy_array, seedname, fig_dimensions
 
 
 def read_nnkp(seedname):
+    """
+    Read the nnkp file to get the reciprocal lattice vectors
+    :param seedname: the name of the system (string)
+    :return: bvec (2darray(dtype=float)) the reciprocal lattice vectors
+    """
     filename = seedname + '.nnkp'
     bvec = np.empty([3, 3])
-    f = open(filename)
-    for line in f:
-        if line == 'begin recip_lattice\n':
-            for i in range(3):
-                bvec[i, :] = f.readline().split()
-    f.close()
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            if line == 'begin recip_lattice\n':
+                for i in range(3):
+                    bvec[i, :] = f.readline().split()
     return bvec
 
 
 def read_kpoints(seedname):
-    f = open('kpoints', 'r')
+    """
+    Make the x-axis for the plot.
+    :param seedname: the name of the system (string)
+    :return: kdists the x-axis for the plot (1darray(dtype=float))
+    """
     bvec = read_nnkp(seedname)
+    f = open('kpoints', 'r')
     kpt_per_path = int(f.readline())
     n_hsym_pts = int(f.readline())
     hsym_pts = np.empty([n_hsym_pts, 3])
@@ -80,13 +106,10 @@ def read_kpoints(seedname):
         abs_hsym_pts[i, :] = hsym_pts[i, 0] * bvec[0, :] \
                              + hsym_pts[i, 1] * bvec[1, :] \
                              + hsym_pts[i, 2] * bvec[2, :]
-    ik = 0
     for i in range(n_hsym_pts - 1):
-        kpath = hsym_pts[i + 1, :] - hsym_pts[i, :]
-        dk = kpath / kpt_per_path
         abs_kpath = abs_hsym_pts[i + 1, :] - abs_hsym_pts[i, :]
         abs_dk = abs_kpath / kpt_per_path
-        for j in range(kpt_per_path):
+        for ik in range(kpt_per_path):
             kdists[ik + 1] = np.linalg.norm(abs_dk) + kdists[ik]
             ik += 1
     kdists -= kdists[kpt_per_path]
@@ -94,6 +117,14 @@ def read_kpoints(seedname):
 
 
 def read_spectral_function(seedname, nene, nkp, nlayers):
+    """
+    Read the input from the spectral function text file
+    :param seedname: the name of the system (string)
+    :param nene: number of energies in the y axis
+    :param nkp:  number of kpoints in the x axis
+    :param nlayers: number of unit-cells used to model the system
+    :return: spectral_function (ndarray(dtype=float))
+    """
     filename = seedname + '_spec_func.dat'
     spectral_function_1d = np.loadtxt(filename)
     spectral_function = np.reshape(spectral_function_1d, (nlayers, nene, nkp),
@@ -103,10 +134,21 @@ def read_spectral_function(seedname, nene, nkp, nlayers):
 
 def plot_spectra(layer1, layer2, spectral_function, klist, omegas, seedname,
                  fig_dims):
+    """
+    Draw the graph and save as raster image.
+    :param layer1: First unit cell
+    :param layer2: Last unit cell
+    :param spectral_function: spectral function - will be plotted
+    :param klist: x-axis
+    :param omegas: y-axis
+    :param seedname: the name of the system (string)
+    :param fig_dims: dimensions of the figure
+    :return: none
+    """
     if layer1 != layer2:
         fig_title = f'Layer = {layer1}-{layer2}'
         filename = f'{seedname}_layer_{layer1}-{layer2}.png'
-        plot_func = (np.sum(spectral_function[layer1-1:layer2, :, :], axis=0))
+        plot_func = np.sum(spectral_function[layer1-1:layer2, :, :], axis=0)
     else:
         layer = layer2
         fig_title = f'Layer = {layer}'
@@ -127,7 +169,7 @@ def plot_spectra(layer1, layer2, spectral_function, klist, omegas, seedname,
 
 def main():
     # Initialise all the variables from the input files
-    nlayers, efermi, energy_array, seedname, fig_dims = read_master_input()
+    nlayers, energy_array, seedname, fig_dims = read_master_input()
     kdists = read_kpoints(seedname)
     nkp = len(kdists)
     nene = len(energy_array)
