34bc8e2fc68aac7e3e4491d66a4e21f1d7ee45e0
Varun Shah <varunshah1801@outlook.com>
2024-08-27 13:52:32 +0100

    fix the position vector

--- peierls_substitution.f90
+++ peierls_substitution.f90
@@ -12,7 +12,6 @@ contains
         integer ::  i, j, k
         real (real64) :: phase, vector_potential(3), r_real(3)
     
-        r_real=0d0
         vector_potential(1)=-1d0*a_0*sin(time*omega)
         vector_potential(2)=a_0*cos(phase_shift+(time*omega))
         vector_potential(3)=0d0
@@ -21,6 +20,7 @@ contains
         ham_t=r_ham
         do i=1, nbands
             do j=1, nbands
+                r_real=0d0
                 do k=1, 3
                     r_real=r_real+(r(k)*avec(k, :))
                 enddo
