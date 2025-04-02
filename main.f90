34fb0f9514d206929b6b9f506e5a545c1216e142
Varun Shah <varunshah1801@outlook.com>
2025-04-02 17:45:19 +0100

    We use zhbevd_2stage now

--- main.f90
+++ main.f90
@@ -9,11 +9,11 @@ implicit none
     integer :: ibeg, iend, pid, ncpus, ierr, extra  ! for mpi
     integer, allocatable :: sendcounts(:), displs(:)! for mpi
     integer, allocatable :: sendcounts_ene(:), displs_ene(:)! for mpi
-    integer (int32):: info, lwork, liwork, lrwork
+    integer (int32):: info, lwork, liwork, lrwork, kd, ldab, ldz
     integer (int32), allocatable :: tiwork(:), iwork(:)
     real (real64), allocatable :: trwork(:), rwork(:), energies(:), omegas(:), &
         energies_glob(:)
-    complex (real64), allocatable :: twork(:), work(:), kham(:, :),            &
+    complex (real64), allocatable :: twork(:), work(:), kham(:, :), ab(:, :),  &
         green_func(:), green_func_glob(:), floquet_ham_list(:, :, :)
 
     ! Initialise MPI here
@@ -26,7 +26,7 @@ implicit none
 
     call read_kpoints ! nkpath, high_sym_pts, nkpt_per_path
     call read_hr ! r_list, r_ham_list, weights, num_r_pts, num_bands, nlayers
-                 ! do_floquet
+                 ! do_floquet, bulk
     call read_potential ! potential
     max_order=0
     if(do_floquet) call read_vector_potential ! vector_potential, max_order
@@ -103,19 +103,38 @@ implicit none
         call ft_ham_r(nf_bands, kp(ik, :), kham, r_list, weights,              &
             floquet_ham_list, num_r_pts, nlayers)
         ! call add_potential(kham, nlayers, num_bands)
-        if(.not.(allocated(work) .and. allocated(rwork)                        &
-            .and. allocated(iwork))) then
+        if (.not.(bulk)) then
+            if(.not.(allocated(ab))) allocate(ab(tot_bands, tot_bands))
+            ab = kham
+            kd = max(0, maxval(r_list(:, direction)))
+            ldab = tot_bands
+            ! 'z' is just kham
+            ldz = tot_bands
+            if(.not.(allocated(work) .and. allocated(rwork)                    &
+                .and. allocated(iwork))) then
+                call ZHBEVD_2STAGE('V', 'U', tot_bands, kd, ab, ldab,          &
+                    energies(ikeneb:ikenee), kham, ldz, twork, lwork, trwork,  &
+                    lrwork, tiwork, liwork, info)
+                allocate(work(lwork), rwork(lrwork), iwork(liwork))
+            endif
+            call ZHBEVD_2STAGE('V', 'U', tot_bands, kd, ab, ldab,              &
+                energies(ikeneb:ikenee), kham, ldz, work, lwork, rwork, lrwork,&
+                iwork, liwork, info)
+        else
+            if(.not.(allocated(work) .and. allocated(rwork)                    &
+                .and. allocated(iwork))) then
+                call zheevd('V', 'L', tot_bands, kham, tot_bands,              &
+                    energies(ikeneb:ikenee), twork, lwork, trwork, lrwork,     &
+                    tiwork, liwork, info) 
+                lwork=int(twork(1))
+                lrwork=int(trwork(1))
+                liwork=tiwork(1)
+                allocate(work(lwork), rwork(lrwork), iwork(liwork))
+            endif
             call zheevd('V', 'L', tot_bands, kham, tot_bands,                  &
-                energies(ikeneb:ikenee), twork, lwork, trwork, lrwork, tiwork, &
-                liwork, info) 
-            lwork=int(twork(1))
-            lrwork=int(trwork(1))
-            liwork=tiwork(1)
-            allocate(work(lwork), rwork(lrwork), iwork(liwork))
+            energies(ikeneb:ikenee), work, lwork, rwork, lrwork, iwork, liwork,&
+            info) ! Just because it's better than zheev for large matrices
         endif
-        call zheevd('V', 'L', tot_bands, kham, tot_bands,                      &
-        energies(ikeneb:ikenee), work, lwork, rwork, lrwork, iwork, liwork,    &
-        info) ! Just because it's better than zheev for large matrices
         if(gfplot) call greens_function(nene, nlayers, nf_bands, omegas, kham, &
             energies(ikeneb:ikenee), eta, green_func(ikgfb:ikgfe))
         if(pid .eq. 0) print*, 'done'
